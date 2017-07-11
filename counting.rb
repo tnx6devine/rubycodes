63be2be4bccfbac0eb309eaa96aa00f864b87933
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-11 15:12:47 -0400

    divisibles and ands

--- counting.rb
+++ counting.rb
@@ -1,8 +1,13 @@
 count = 0
 100.times do
 count+=1
-if count.%(3).zero?
+if count % 3 == 0 && count % 5 == 0
+	p "Mined Minds"
+elsif count % 3 == 0
 p "Mined"	
+elsif count % 5 == 0
+	p "Minds"
+
 else
 	p count
 end
